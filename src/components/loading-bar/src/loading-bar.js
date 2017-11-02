3d7185152efe1cd4c49f490ee134b970baa00296
koppthe <kopptsoft@gmail.com>
2017-11-02 14:47:41 +0800

    fix: width option doesn't work (#35)

--- src/components/loading-bar/src/loading-bar.js
+++ src/components/loading-bar/src/loading-bar.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import LoadingBarVue from './loading-bar.vue'
 
 const LoadingBarConstructor = Vue.extend(LoadingBarVue)
-let instance
+
+let loadingBarInstance, instance, timer
+let width = 2
 
 const LoadingBar = options => {
   options = options || {}
@@ -31,4 +33,93 @@ LoadingBar.prototype.destroy = () => {
   document.body.removeChild(instance.vm.$el)
 }
 
-export default LoadingBar
+function getLoadingBarInstance () {
+  loadingBarInstance = loadingBarInstance || new LoadingBar({ width })
+  return loadingBarInstance
+}
+
+function clearTimer () {
+  if (timer) {
+    clearInterval(timer)
+    timer = null
+  }
+}
+
+function update (options) {
+  const barInstance = getLoadingBarInstance()
+  barInstance.update(options)
+}
+
+function hide () {
+  setTimeout(() => {
+    update({
+      percent: 0,
+      show: false
+    })
+    destroy()
+  }, 800)
+}
+
+function destroy () {
+  const barInstance = getLoadingBarInstance()
+  clearTimer()
+  loadingBarInstance = null
+  barInstance.destroy()
+}
+
+export default {
+  config (options) {
+    if (options.width) {
+      width = options.width
+    }
+  },
+  start () {
+    if (timer) return
+
+    let percent = 0
+
+    update({
+      percent,
+      status: 'success',
+      show: true
+    })
+
+    timer = setInterval(() => {
+      percent += Math.floor((Math.random() * 3) + 5)
+      if (percent > 95) {
+        clearTimer()
+      }
+      update({
+        percent,
+        status: 'success',
+        show: true
+      })
+    }, 200)
+  },
+  update (percent) {
+    clearTimer()
+    update({
+      percent,
+      status: 'success',
+      show: true
+    })
+  },
+  finish () {
+    clearTimer()
+    update({
+      percent: 100,
+      status: 'success',
+      show: true
+    })
+    hide()
+  },
+  error () {
+    clearTimer()
+    update({
+      percent: 100,
+      status: 'error',
+      show: true
+    })
+    hide()
+  }
+}
